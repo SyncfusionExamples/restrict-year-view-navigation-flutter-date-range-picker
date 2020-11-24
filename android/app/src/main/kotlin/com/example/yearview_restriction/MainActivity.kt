25e497ead43a99eaf5cb38cba5b497aa02ccf7b7
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-11-24 17:46:00 +0530

    FLUT-2995 - Sample added.

---
+++ android/app/src/main/kotlin/com/example/yearview_restriction/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.yearview_restriction
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_datepicker/datepicker.dart';
+
+void main() => runApp(RangeSelection());
+
+class RangeSelection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          body: Card(
+              margin: const EdgeInsets.fromLTRB(50, 150, 50, 100),
+              child: SfDateRangePicker(
+                view: DateRangePickerView.month,
+                allowViewNavigation: true,
+              ))
+
+          // This trailing comma makes auto-formatting nicer for build methods.
+          ),
+    );
+  }
+}
